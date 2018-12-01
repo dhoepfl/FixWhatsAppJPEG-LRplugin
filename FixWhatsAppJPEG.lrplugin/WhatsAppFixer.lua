35ca6cec98cec3641c9dd541a3c8e130f7997292
Daniel Höpfl <daniel@hoepfl.de>
2018-12-01 19:56:24 +0100

    Added quotes to tool path

--- FixWhatsAppJPEG.lrplugin/WhatsAppFixer.lua
+++ FixWhatsAppJPEG.lrplugin/WhatsAppFixer.lua
@@ -13,9 +13,9 @@ local WhatsAppFixer = {}
 function WhatsAppFixer.fixPhoto(photo)
    local command
    if WIN_ENV == true then
-      command = LrPathUtils.child(LrPathUtils.child( _PLUGIN.path, "win" ), "fixWhatsApp.exe" )
+      command = '"' .. LrPathUtils.child(LrPathUtils.child( _PLUGIN.path, "win" ), "fixWhatsApp.exe" ) .. '"'
    else
-      command = LrPathUtils.child(LrPathUtils.child( _PLUGIN.path, "mac" ), "fixWhatsApp" )
+      command = "'" .. LrPathUtils.child(LrPathUtils.child( _PLUGIN.path, "mac" ), "fixWhatsApp" ) .. "'"
    end
 
    if LrTasks.execute(command .. " '" .. photo.path .. "' '" .. photo.path .. "'") == 0 then
